1ef77216090bb45dd1b4d5341796f3faf4522ab0
Breno Nascimento <satsukebare23@gmail.com>
2022-11-09 14:53:45 -0300

    add rot for create new classroom

--- backEnd/src/routes/module.routes.ts
+++ backEnd/src/routes/module.routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import validationModule from '../controllers/authValidation/moduleValidation';
+import ClassroomsController from '../controllers/Classrooms.controller';
 import ModuleController from '../controllers/Module.controller';
 import validateAdm from '../middlewares/adm.middleware';
 import validateToken from '../middlewares/tokenVerification';
@@ -7,6 +8,7 @@ import validateToken from '../middlewares/tokenVerification';
 const routes = express.Router();
 
 const moduleController = new ModuleController();
+const classRoomController = new ClassroomsController();
 
 routes.use(validateToken);
 
@@ -14,7 +16,7 @@ routes.get('/', moduleController.getAllModules);
 routes.get('/sub', moduleController.getAllSubModules);
 routes.get('/classrooms', moduleController.getClassrooms);
 
-routes.post('/classrooms',validateAdm, moduleController.getClassrooms);
+routes.post('/classrooms',validateAdm, classRoomController.addNewClassroom);
 routes.post('/',validateAdm, validationModule, moduleController.addNewModule);
 routes.delete('/:id',validateAdm,validationModule, moduleController.deleteModule);
 routes.put('/:id',validateAdm, validationModule, moduleController.updateModule);
