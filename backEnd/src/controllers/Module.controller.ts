8430ee011cb635d3fe8edace3c7d7a2ee3968329
Breno Nascimento <satsukebare23@gmail.com>
2022-11-08 22:36:02 -0300

    create getAllFree classrooms

--- backEnd/src/controllers/Module.controller.ts
+++ backEnd/src/controllers/Module.controller.ts
@@ -9,6 +9,14 @@ class ModuleController {
     const modules = await this.bookService.getAll();
     res.status(statusCodes.OK).json({ message: modules, status: 'Sucesso!' })
   }
+
+  public getAllFree = async (_req: Request, res: Response) => {
+    const { error, message } = await this.bookService.findFreeClassroom();
+    if(error) return res.status(statusCodes.BAD_REQUEST)
+      .json({ message });
+
+    return res.status(statusCodes.OK).json({ message });
+  }
 }
 
 export default ModuleController;
