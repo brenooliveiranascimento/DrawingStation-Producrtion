326753e4e0c47d792f95a767a09a3bc27c9a124c
Breno Nascimento <satsukebare23@gmail.com>
2022-11-12 15:19:38 -0300

    add modules reducer in aplication

--- frontend/src/redux/modules/rootReducer.ts
+++ frontend/src/redux/modules/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import user from '../modules/user/user';
+import modules from '../modules/modules/modules';
 
 const rootReducer = combineReducers({
-  user
+  user,
+  modules
 });
 
 export default rootReducer;
