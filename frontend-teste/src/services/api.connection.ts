ee46c06f66b0f3720e9f69814bf05d593d0cfa24
Breno Nascimento <satsukebare23@gmail.com>
2022-11-12 12:25:30 -0300

    remove old frontend

--- frontend-teste/src/services/api.connection.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const apiConnection = axios.create({
-  baseURL: 'http://localhost:8000',
-  // baseURL: 'https://solid-page-production.up.railway.app/',
-});
-
-export default apiConnection
