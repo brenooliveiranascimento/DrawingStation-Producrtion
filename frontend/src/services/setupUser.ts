9509e13329e2f4b6f69b6617c21c9aed1110d9e4
Breno Nascimento <satsukebare23@gmail.com>
2022-11-16 22:49:25 -0300

    add railway endpoiint 2

--- frontend/src/services/setupUser.ts
+++ frontend/src/services/setupUser.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 export const setupUser = (ctx: any) => {
   const cookies = parseCookies(ctx);
   const api = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://backend-production-8de5.up.railway.app/',
     headers: {
       Authorization: `${cookies['DRAWING_USER_DATA']}`
     }
