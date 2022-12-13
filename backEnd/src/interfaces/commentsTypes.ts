0a256d8c05b6561989c76ea189e1fd4963048110
Breno Nascimento <satsukebare23@gmail.com>
2022-12-13 19:56:34 -0300

    create comment interface

---
+++ backEnd/src/interfaces/commentsTypes.ts
@@ -0,0 +1,15 @@
+export interface subComments {
+  id: number;
+  content: string;
+  createBy: string;
+  commentId: boolean
+  creationData: Date;
+}
+
+export interface Comments {
+  id: number;
+  content: string;
+  createBy: string;
+  classroomId: boolean
+  creationData: Date;
+}
