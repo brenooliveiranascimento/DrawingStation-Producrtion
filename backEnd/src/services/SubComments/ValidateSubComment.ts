4d22e68b61b08e564501c18c4459ff288d81924d
Breno Nascimento <satsukebare23@gmail.com>
2022-12-14 16:15:32 -0300

    create validate subcomment class

---
+++ backEnd/src/services/SubComments/ValidateSubComment.ts
@@ -0,0 +1,17 @@
+import { IsubComments } from "../../interfaces/commentsTypes";
+import { errorMapTypes } from "../../utils/errorMap";
+import CustomError from "../../utils/StatusError";
+import CommentModel from "../../database/models/CommentModel";
+import Users from "../../database/models/UserModel";
+
+export default class ValidateSubComment {
+  async checkNewSubComment(subComment: IsubComments) {
+    const { commentId, content, userId } = subComment;
+    const comment = await CommentModel.findByPk(Number(commentId));
+    const user = await Users.findByPk(Number(userId));
+
+    if(!content.length) throw new CustomError(errorMapTypes.COMMENT_NO_CONTENT, 401);
+    if(!commentId || !comment) throw new CustomError(errorMapTypes.COMMENT_NOT_FOUND, 404);
+    if(!user || !user) throw new CustomError(errorMapTypes.USER_DONT_EXIST, 404);
+  }
+}
