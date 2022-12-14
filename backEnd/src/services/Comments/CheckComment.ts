027836616c7d08dec920beabd6d4606f80a32c6a
Breno Nascimento <satsukebare23@gmail.com>
2022-12-14 14:09:53 -0300

    create checo comment

--- backEnd/src/services/Comments/CheckComment.ts
+++ backEnd/src/services/Comments/CheckComment.ts
@@ -10,12 +10,13 @@ export default class CheckComment {
     private usersModel = Users,
   ) {}
 
-  async commentCheckList(comment: IComments): Promise<void> {
+  async commentCheckList(comment: IComments): Promise<boolean> {
     const { content, userId, classroomId } = comment;
     const user = await this.usersModel.findByPk(userId);
     const classroom = await this.classroomModel.findByPk(classroomId);
     if(!content) throw new CustomError(errorMapTypes.COMMENT_NO_CONTENT, 401);
     if(!userId || !user) throw new CustomError(errorMapTypes.USER_COMMENT_NOT_FOUND, 401);
     if(!classroomId || !classroom) throw new CustomError(errorMapTypes.CLASSROOM_COMMENT_NOT_FOUND, 401);
+    return true
   }
 };
--- backEnd/src/services/Comments/UpdateComment.ts
+++ backEnd/src/services/Comments/UpdateComment.ts
@@ -1,16 +1,23 @@
 import CommentModel from "../../database/models/CommentModel";
 import { ICommentGenericReturn, IComments } from "../../interfaces/commentsTypes";
+import { errorMapTypes } from "../../utils/errorMap";
+import CustomError from "../../utils/StatusError";
+import CheckComment from "./CheckComment";
 
 export default class UpdateComment {
-  constructor(private commentModel = CommentModel) {}
+  constructor(
+    private commentModel = CommentModel,
+    private checkComment = CheckComment
+    ) {}
 
   async execute(comment: IComments): Promise<ICommentGenericReturn> {
     const { id } = comment;
     try {
+      await this.checkComment
       await this.commentModel.update({ ...comment }, {where: { id }})
       return { message: 'Comentário atualizado com sucesso!' };
     } catch(e: any) {
-      throw new Error()
+      throw new CustomError('Server Error, não foi possível atualizar o comentário', 404);
     }
   }
 }
