ad00f8a84a79026248812f9bb3d846f4250b3488
Breno Nascimento <satsukebare23@gmail.com>
2022-12-14 16:16:01 -0300

    add type return in check new subcomment

--- backEnd/src/services/SubComments/ValidateSubComment.ts
+++ backEnd/src/services/SubComments/ValidateSubComment.ts
@@ -5,7 +5,7 @@ import CommentModel from "../../database/models/CommentModel";
 import Users from "../../database/models/UserModel";
 
 export default class ValidateSubComment {
-  async checkNewSubComment(subComment: IsubComments) {
+  async checkNewSubComment(subComment: IsubComments): Promise<boolean> {
     const { commentId, content, userId } = subComment;
     const comment = await CommentModel.findByPk(Number(commentId));
     const user = await Users.findByPk(Number(userId));
@@ -13,5 +13,6 @@ export default class ValidateSubComment {
     if(!content.length) throw new CustomError(errorMapTypes.COMMENT_NO_CONTENT, 401);
     if(!commentId || !comment) throw new CustomError(errorMapTypes.COMMENT_NOT_FOUND, 404);
     if(!user || !user) throw new CustomError(errorMapTypes.USER_DONT_EXIST, 404);
+    return true
   }
 }
