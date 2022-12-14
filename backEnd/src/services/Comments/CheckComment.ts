6f384b1ba38aee8fbe0265cc3a4eeb31f6110b46
Breno Nascimento <satsukebare23@gmail.com>
2022-12-14 17:03:23 -0300

    best format in comments validation

--- backEnd/src/services/Comments/CheckComment.ts
+++ backEnd/src/services/Comments/CheckComment.ts
@@ -12,9 +12,10 @@ export default class CheckComment {
   async commentCheckList(comment: IComments): Promise<boolean> {
     const { content, userId, classroomId } = comment;
     const user = await Users.findByPk(userId);
+    if(!userId || !user) throw new CustomError(errorMapTypes.USER_COMMENT_NOT_FOUND, 401);
+
     const classroom = await Classroom.findByPk(classroomId);
     if(!content) throw new CustomError(errorMapTypes.COMMENT_NO_CONTENT, 401);
-    if(!userId || !user) throw new CustomError(errorMapTypes.USER_COMMENT_NOT_FOUND, 401);
     if(!classroomId || !classroom) throw new CustomError(errorMapTypes.CLASSROOM_COMMENT_NOT_FOUND, 401);
     return true
   }
@@ -22,11 +23,12 @@ export default class CheckComment {
   async commentUpdateCheckList(comment: ICommentUpdate): Promise<boolean> {
     const { content, userId, id } = comment;
     const getComment = await CommentModel.findByPk(id);
+    if(!getComment) throw new CustomError(errorMapTypes.COMMENT_NOT_FOUND, 404);
+    if(userId !== Number(getComment.userId)) throw new CustomError(errorMapTypes.COMMENT_USER_DONT_MATCH, 401);
+
     const user = await Users.findByPk(userId);
     if(!user) throw new CustomError(errorMapTypes.USER_DONT_EXIST, 401);
-    if(!getComment) throw new CustomError(errorMapTypes.COMMENT_NOT_FOUND, 404);
     if(!content) throw new CustomError(errorMapTypes.COMMENT_NO_CONTENT, 401);
-    if(userId !== Number(getComment.userId)) throw new CustomError(errorMapTypes.COMMENT_USER_DONT_MATCH, 401);
     return true
   }
 };
