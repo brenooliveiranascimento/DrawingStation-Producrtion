a154c522b6dbc22b8a33f1b40561385944e47259
Breno Nascimento <satsukebare23@gmail.com>
2022-12-14 16:24:46 -0300

    create subCommentController class

---
+++ backEnd/src/services/SubComments/SubComment.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { ICommentGenericReturn, IsubComments } from "../../interfaces/commentsTypes";
+
+interface ISubcommentControllerProps {
+  create: { execute: (subComment: IsubComments) => Promise<ICommentGenericReturn> }
+}
+
+export default class SubCommentControlelr {
+  constructor(private props: ISubcommentControllerProps) {
+    this.props = props;
+  }
+
+  async create(req: Request, res: Response) {
+    const subComment: IsubComments = req.body;
+    const create = this.props.create.execute(subComment);
+    res.status(201).json(create);
+  }
+}
