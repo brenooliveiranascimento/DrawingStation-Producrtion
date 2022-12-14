425f57c3db0c3f35b70879c3773c40c4e4d04ce0
Breno Nascimento <satsukebare23@gmail.com>
2022-12-14 18:17:36 -0300

    add delete subCOmment funcionality

--- backEnd/src/routes/subComments.routes.ts
+++ backEnd/src/routes/subComments.routes.ts
@@ -2,17 +2,20 @@ import { Router } from 'express';
 import SubCommentControlelr from '../services/SubComments/SubComment.controller';
 import CreateSubComment from '../services/SubComments/CreateSubComment';
 import UpdateSubComment from '../services/SubComments/UpdateSubComment';
+import DeleteSubComment from '../services/SubComments/DeleteSubComment';
 
 const router = Router();
 
 const subCommentServices = {
   create: new CreateSubComment(),
   update: new UpdateSubComment(),
+  delete: new DeleteSubComment(),
 }
 
 const subCommentControlelr = new SubCommentControlelr(subCommentServices);
 
 router.post('/create', (req, res) => subCommentControlelr.create(req, res));
 router.post('/update/:id', (req, res) => subCommentControlelr.update(req, res));
+router.delete('/delete/:id', (req, res) => subCommentControlelr.delete(req, res));
 
 export default router;
