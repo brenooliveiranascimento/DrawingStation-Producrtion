530ed876380bbdfab02f455de7a5eeb1706e105e
Breno Nascimento <satsukebare23@gmail.com>
2022-12-15 15:06:55 -0300

    create susbcription rote in frontend

--- backEnd/src/services/Subscriptions/SubscriptionService.ts
+++ backEnd/src/services/Subscriptions/SubscriptionService.ts
@@ -1,6 +1,7 @@
 import Subscription from "../../database/models/Subscription";
 import UserModel from '../../database/models/UserModel';
 import Stripe from 'stripe';
+import { stripe } from "../../utils/stripe";
 import 'dotenv';
 
 interface IExecuteSubs {
@@ -8,27 +9,40 @@ interface IExecuteSubs {
 }
 
 export default class SubscriptionService {
+
   async execute({ userId }: IExecuteSubs) {
+    const user = await UserModel.findByPk(
+      userId, { attributes: { exclude: ['password'] } });
 
+      let customerId = user?.stripeClientId;
+      
+    if(!user?.stripeClientId) {
+      const stripeCustomers = await stripe.customers.create({
+        email: user?.email.toString(),
+      });
 
-    const stripeKey = process.env.STRIPE_API_KEY as string;
+      await UserModel.update(
+        { stripeClientId: customerId },
+        { where: { id: userId } }
+      )
+      customerId = stripeCustomers.id;
+    }
 
-    const stripe = new Stripe(
-      stripeKey,
-      {
-        apiVersion: '2022-11-15',
-        appInfo: {
-          name: 'DrawingStation',
-          version: '1'
-        }
-      }
-    );
-    
+    const successUrl = process.env.STRIPE_SUCCESS_URL as string
+    const failUrl = process.env.STRIPE_CANCEL_URL as string
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        { price: process.env.STRIPE_PRICE, quantity: 1 }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: successUrl,
+      cancel_url: failUrl
+    })
 
-    const user = await UserModel.findByPk(userId, {
-      attributes: { exclude: ['password'] }
-    });
-
-    return user
+    return { sessionId: stripeCheckoutSession.id }
   }
 }
---
+++ frontEnd/src/pages/Subscription/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function index() {
+  return (
+    <section>
+      <button>
+        Assianr plano mensal
+      </button>
+    </section>
+  );
+}
