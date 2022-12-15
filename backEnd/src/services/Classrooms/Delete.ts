a90f666cad975779f3f06124ac64466d2b93820d
Breno Nascimento <satsukebare23@gmail.com>
2022-12-15 10:02:43 -0300

    create delete classroom class

---
+++ backEnd/src/services/Classrooms/Delete.ts
@@ -0,0 +1,22 @@
+import statusCodes from "../../statusCode";
+import { errorMapTypes } from "../../utils/errorMap";
+import CustomError from "../../utils/StatusError";
+import Classroom from "./Classroom";
+import ClassroomModel from "../../database/models/ClassroomModel";
+
+export default class Delete extends Classroom {
+  constructor() { super() }
+
+  public async deleteClassroom(id: number) {
+    try {
+      const classroom = await this.findClassroomById(id);
+      if(classroom) throw new CustomError(errorMapTypes.CLASSROOM_DONT_EXIST, statusCodes.NOT_FOUND);
+
+      await ClassroomModel.destroy({where: { id }});
+
+      return { message: 'Aula Deletada com sucesso!' };
+    } catch(e) {
+      return { error: { message: errorMapTypes.REQUEST_ERROR }, message: e };
+    }
+  }
+}
