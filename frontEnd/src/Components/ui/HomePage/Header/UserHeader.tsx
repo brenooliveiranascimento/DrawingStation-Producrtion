b5b089c1a2b54f10de044ad89a42a5e5b6e7c8e8
Breno Nascimento <satsukebare23@gmail.com>
2022-12-17 19:47:22 -0300

    create user header

--- frontEnd/src/Components/ui/HomePage/Header/UserHeader.tsx
+++ frontEnd/src/Components/ui/HomePage/Header/UserHeader.tsx
@@ -1,11 +1,24 @@
+import Image from 'next/image';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { globalState } from '../../../../interfaces/modules/globalStateInterface';
 import styles from './styles.module.scss';
+import profileDefault from '../../../../../public/profilePhoto.png';
 
 export default function UserHeader() {
+  const { userData } = useSelector((state: globalState) => state.user);
+  const { name, premium, profilePhoto } = userData;
   return (
     <header className={styles.header_container}>
-      <h1>a</h1>
-      <h1>a</h1>
+      <aside>
+        <h1>a</h1>
+      </aside>
+      <aside>
+        <span>{name}</span>
+        <Image width={50} alt={name} height={50} style={{
+          borderRadius: '50%' 
+        }} src={profilePhoto || profileDefault}/>
+      </aside>
     </header>
   );
 }
