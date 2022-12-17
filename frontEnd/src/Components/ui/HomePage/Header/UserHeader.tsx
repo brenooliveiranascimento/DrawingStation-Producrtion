0771906d72906d78be875af419a642bbef447357
Breno Nascimento <satsukebare23@gmail.com>
2022-12-17 20:04:17 -0300

    create premium btn

--- frontEnd/src/Components/ui/HomePage/Header/UserHeader.tsx
+++ frontEnd/src/Components/ui/HomePage/Header/UserHeader.tsx
@@ -7,20 +7,25 @@ import profileDefault from '../../../../../public/profilePhoto.png';
 
 export default function UserHeader() {
   const { userData } = useSelector((state: globalState) => state.user);
-  const { name, premium, profilePhoto } = userData;
+  const { name, premium, profilePhoto, stripeClientId } = userData;
   return (
     <header className={styles.header_container}>
       <aside>
         <h1>a</h1>
       </aside>
       <aside>
-        <span>{name}</span>
-        <section>
+        <section className={styles.premium_area}>
+          <h2>{name}</h2>
+          <button>
+            <span>{ stripeClientId ? 'Renovar Assinatura' : 'Obter Premium' }</span>
+          </button>
+        </section>
+        <button>
           <Image width={50} alt={name} height={50} style={{
             borderRadius: '50%' , border: premium ? '4px solid #B8860B' : 'null'
           }} src={profilePhoto || profileDefault}/>
-          <span>Premium!</span>
-        </section>
+          { premium && <span>Premium!</span> }
+        </button>
       </aside>
     </header>
   );
