00a858a5a8d5452f38d6290031b69f1c45ea01f6
Breno Nascimento <satsukebare23@gmail.com>
2022-12-19 18:42:52 -0300

    crea errors validations in init recover password service

--- backEnd/src/services/AccountValidation/InitRecoverPassword.ts
+++ backEnd/src/services/AccountValidation/InitRecoverPassword.ts
@@ -2,8 +2,22 @@ import UserModel from "../../database/models/UserModel";
 import { errorMapTypes } from "../../utils/errorMap";
 import CustomError from "../../utils/StatusError";
 import jwt from "jsonwebtoken";
+import statusCodes from "../../statusCode";
 
 export default class InitRecoverPassword {
+
+  private async validateToken(token: string, email: string) {
+    const key = process.env.SECRET as string
+  
+    if (!token) throw new CustomError(errorMapTypes.TOKEN_NOT_FOUND, statusCodes.UNAUTHORIZED);
+    try {
+      jwt.verify(token, key);
+      return true
+    } catch (err) {
+      UserModel.update({ recoverPassword: null }, { where: { email } });
+      throw new CustomError(errorMapTypes.REQUEST_ERROR, statusCodes.BAD_REQUEST);
+    }
+  };
 
   private createToken(email: string) {
     const key = process.env.SECRET as string
@@ -13,10 +27,12 @@ export default class InitRecoverPassword {
 
   async execute(email: string) {
     try { 
-      const userExist = await UserModel.findOne({ where: { email } })
+      const userExist = await UserModel.findOne({ where: { email } });
       if(!userExist) throw new CustomError(errorMapTypes.USER_DONT_EXIST, 500);
-
-      await UserModel.update({ recoverPassword: this.createToken(email) }, { where: { email } });
+      const token = this.createToken(email);
+      await this.validateToken(token, email);
+      await UserModel.update({ recoverPassword: token }, { where: { email } });
+      return 'Token criado com sucesso!';
     } catch(e: any) {
       throw new CustomError(e.message, 500);
     }
