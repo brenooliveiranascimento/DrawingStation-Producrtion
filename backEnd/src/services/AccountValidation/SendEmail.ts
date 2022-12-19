12d43acdb174e8c5a0b1e95ee9973e08c37fdd95
Breno Nascimento <satsukebare23@gmail.com>
2022-12-19 12:37:16 -0300

    create first send email service

---
+++ backEnd/src/services/AccountValidation/SendEmail.ts
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer';
+import CustomError from '../../utils/StatusError';
+
+export default class SendEmail {
+  async execute(email: string) {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.hostinger.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.SEND_EMAIL,
+        pass: process.env.SEND_EMAIL_PASSWORD
+      }
+    })
+
+    try {
+      await transporter.sendMail({
+        from: `DrawingStation account verification <${process.env.SEND_EMAIL}>`,
+        to: email,
+        subject: 'Email de verificação da conta',
+        html: '<h1>Olá desenhista!!<h1><p>Isso pe um teste!!!<p>'
+      })
+      return 'Enviado com sucesso!!!'
+    } catch(e) {
+      throw new CustomError('Erro eo enviar o email', 500);
+    }
+  }
+}
