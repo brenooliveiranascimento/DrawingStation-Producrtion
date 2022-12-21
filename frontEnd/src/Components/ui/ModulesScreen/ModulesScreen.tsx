4b607a801bce7cd838c83851a59ee81b3c8b68d6
Breno Nascimento <satsukebare23@gmail.com>
2022-12-21 20:47:57 -0300

    move module card file

--- frontEnd/src/Components/ui/ModulesScreen/ModulesScreen.tsx
+++ frontEnd/src/Components/ui/ModulesScreen/ModulesScreen.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { globalState } from '../../../interfaces/modules/globalStateInterface';
 import { ModulesInterface } from '../../../interfaces/modules/ModulesInterface';
-import ModuleCard from '../ModuleCard/ModuleCard';
+import ModuleCard from './ModuleCard';
 import styles from './styles.module.scss';
 
 export default function ModulesScreen() {
   const { modules } = useSelector((state: globalState) => state);
   return (
-    <section className={styles.module_card_container}>
+    <section className={styles.module_container}>
       { modules.modules.map((currModule: ModulesInterface) => (
         <ModuleCard moduleCard={currModule} key={currModule.id}/>
       )) }
