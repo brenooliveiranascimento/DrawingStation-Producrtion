b3a6af0701003884f307827364945e7b404be508
Breno Nascimento <satsukebare23@gmail.com>
2022-12-23 17:05:30 -0300

    add class controller

--- frontEnd/src/Components/ui/Player/player.tsx
+++ frontEnd/src/Components/ui/Player/player.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import Router, { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { ClassroomDataInterface } from '../../../interfaces/modules/classroomInterface';
+import { globalState } from '../../../interfaces/modules/globalStateInterface';
 import styles from './styles.module.scss';
 
 export default function Player() {
+  const { classroomsData } = useSelector(({classroomsData}: globalState) => classroomsData);
+  const [id, setId] = useState<any>('');
+  const Route = useRouter();
+  const videoId = () => {
+    const getId = classroomsData.find((currClassroom: ClassroomDataInterface) => Number(currClassroom.id) === Number(Router.query.classId));
+    setId(getId?.video);
+  };
+  useEffect(() => {
+    videoId();
+  }, [Router.query.classId]);
+
   return (
     <section className={styles.player}>
-      <iframe
-        className="Video"
-        src={'https://www.youtube.com/embed/'}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="Disabled youtube"
-      />
+      <iframe width="560" height="315"
+        src={`https://www.youtube.com/embed/${id}`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      ></iframe>
     </section>
   );
 }
