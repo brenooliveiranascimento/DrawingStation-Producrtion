a622e549616ee2cc8774a986a5638a6e9903f582
Breno Nascimento <satsukebare23@gmail.com>
2022-12-24 16:58:16 -0300

    corrigindo verbo http

--- frontEnd/src/redux/actions/commentsActions/deleteComment.ts
+++ frontEnd/src/redux/actions/commentsActions/deleteComment.ts
@@ -1,11 +1,12 @@
 import { parseCookies } from 'nookies';
 import { Dispatch } from 'react';
 import { toast } from 'react-toastify';
-import { IDeleteComment, IEditComment } from '../../../interfaces/modules/commentsModuleInterfaces';
+import { ICommentsWithUserData, IDeleteComment, IEditComment } from '../../../interfaces/modules/commentsModuleInterfaces';
 import { apiConnection } from '../../../services/api.connection';
-import { requestComments } from './genericAtions';
+import { CommentsTypes } from '../../Types/AuthTypes';
+import { genericCommentAciton, requestComments } from './genericAtions';
 
-export const deleteCommentAction = (commentData: IDeleteComment): any => {
+export const deleteCommentAction = (commentData: IDeleteComment, comentData: ICommentsWithUserData): any => {
   return async (dispatch: Dispatch<any>) => {
     const { userId, id } = commentData;
     const cookies = parseCookies();
@@ -19,7 +20,7 @@ export const deleteCommentAction = (commentData: IDeleteComment): any => {
       toast.success(data);
       const { data: newComments } = await apiConnection.get('/comments/all', { headers: { 'Authorization': token } });
       console.log(newComments);
-      dispatch(requestComments(newComments.reverse()));
+      dispatch(genericCommentAciton(CommentsTypes.DELETE_COMMENT, comentData));
     } catch(e: any) {
       toast.error(e.response.data.message);
     }
