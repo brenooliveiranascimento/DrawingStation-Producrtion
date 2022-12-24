b2eea3f9d52bf9f735e61d372f84f569f485d194
Breno Nascimento <satsukebare23@gmail.com>
2022-12-24 16:49:50 -0300

    add edit comment funcionality

--- frontEnd/src/Components/ui/SubCommentCard/SubCommentCard.tsx
+++ frontEnd/src/Components/ui/SubCommentCard/SubCommentCard.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { IsubComments } from '../../../interfaces/modules/commentsModuleInterfaces';
+import { globalState } from '../../../interfaces/modules/globalStateInterface';
+import { editCommentAction, editSubCommentAction } from '../../../redux/actions/commentsActions/editComment';
 import CommentCardHeader from '../CommentCard/CommentCardHeader';
 
 interface ISubCommentCardInterface {
@@ -7,11 +10,37 @@ interface ISubCommentCardInterface {
 }
 
 export default function SubCommentCard({subComment}: ISubCommentCardInterface) {
+  const [editedValue, setEditedValue] = useState('');
+  const [edit, setEdit] = useState(false);
+  const dispatch = useDispatch();
+
+  const { userData } = useSelector(({ user }: globalState) => user);
+
+  const handleEdit = () => {
+    if(!edit) {
+      setEdit(true);
+      setEditedValue(subComment.content);
+    } else {
+      dispatch(editSubCommentAction({
+        content: editedValue,
+        id: Number(subComment.id),
+        userId: Number(userData.id) }));
+      setEdit(false);
+    }
+  };
+
   return (
     <section>
       <CommentCardHeader userData={subComment.userData} />
       <article>
-        {subComment.content}
+        {edit ? <input
+          onChange={({target}) => setEditedValue(target.value)}
+          value={editedValue}
+        /> :subComment.content}
+        { subComment.userData.id ===  userData.id &&
+        <button onClick={handleEdit}>
+          { edit ? 'Salvar' : 'Editar' }
+        </button>}
       </article>
     </section>
   );
--- frontEnd/src/redux/actions/commentsActions/editComment.ts
+++ frontEnd/src/redux/actions/commentsActions/editComment.ts
@@ -18,8 +18,31 @@ export const editCommentAction = (commentData: IEditComment): any => {
         { headers: { 'Authorization': token } });
       toast.success(data);
       const { data: newComments } = await apiConnection.get('/comments/all', { headers: { 'Authorization': token } });
+      console.log(newComments);
       dispatch(requestComments(newComments.reverse()));
     } catch(e: any) {
+      toast.error(e.response.data.message);
+    }
+  };
+};
+
+export const editSubCommentAction = (commentData: IEditComment): any => {
+  return async (dispatch: Dispatch<any>) => {
+    const { id, content, userId } = commentData;
+    const cookies = parseCookies();
+
+    const token = cookies['DRAWING_USER_DATA'];
+
+    try {
+      const { data } = await apiConnection.post(`/subComments/update/${id}`,
+        { content, userId, },
+        { headers: { 'Authorization': token } });
+      toast.success(data);
+      const { data: newComments } = await apiConnection.get('/comments/all', { headers: { 'Authorization': token } });
+      console.log(newComments);
+      dispatch(requestComments(newComments.reverse()));
+    } catch(e: any) {
+      console.log(e);
       toast.error(e.response.data.message);
     }
   };
