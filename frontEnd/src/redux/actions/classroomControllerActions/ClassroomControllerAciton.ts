b92f0dd06daf50e3426bd3ae3f5a35234897b7fe
Breno Nascimento <satsukebare23@gmail.com>
2022-12-24 11:58:15 -0300

    add localstorage keys update in select module

--- frontEnd/src/redux/actions/classroomControllerActions/ClassroomControllerAciton.ts
+++ frontEnd/src/redux/actions/classroomControllerActions/ClassroomControllerAciton.ts
@@ -1,18 +1,23 @@
 import { Dispatch } from 'react';
 import { globalState } from '../../../interfaces/modules/globalStateInterface';
 import { ModulesInterface, SubModuleInterface } from '../../../interfaces/modules/ModulesInterface';
+import { localStorageKeys } from '../../Types/localStorageTypes';
 import { incompleteASubModule, setCurrClass, setCurrModule, setCurrSubmodule } from '../genericActions';
 // | any = {name: 'Incompleto', id: 1}
 export const selectFirstClassroom = (firstSubModule: SubModuleInterface): any => {
   return async (dispatch: Dispatch<any>) => {
     const { name, id } = firstSubModule;
     dispatch(setCurrSubmodule({ name, id }));
-    dispatch(setCurrClass(firstSubModule.classrooms[0]));
+    const firstClassroom = firstSubModule.classrooms[0];
+    localStorage.setItem(localStorageKeys.lastSubModule, JSON.stringify(id));
+    localStorage.setItem(localStorageKeys.lastClassroom, JSON.stringify(firstClassroom.id));
+    dispatch(setCurrClass(firstClassroom));
   };
 };
 
 export const selectSubModuleAction = (module: ModulesInterface): any => {
   const { name, id } = module;
+  localStorage.setItem(localStorageKeys.lastModule, JSON.stringify(id));
   return async (dispatch: Dispatch<any>, state: () => globalState) => {
     const { subModules } = state().subModules;
     const currSubModules = subModules.filter((currSubModule: SubModuleInterface) =>
