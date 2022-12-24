95176779191be464037ad42a8fb2b53bf8a137ad
Breno Nascimento <satsukebare23@gmail.com>
2022-12-24 10:59:09 -0300

    create classroom controller default value

--- frontEnd/src/redux/modules/classController/classcontroller.ts
+++ frontEnd/src/redux/modules/classController/classcontroller.ts
@@ -1,4 +1,13 @@
-
+const CLASSROOM_CONTROLLER_DEFAULT_VALUE = {
+  classroom: {
+    name: '',
+    premium: true,
+    video: '',
+    description: '',
+    colors: null,
+    image: '',
+  }
+};
 
 export function classroomController(state: any, action: any) {
   switch(action.type) {
