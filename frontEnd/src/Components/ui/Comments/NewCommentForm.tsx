8dfcaddbff2d6b60c90281d14788ea46850cf485
Breno Nascimento <satsukebare23@gmail.com>
2022-12-24 15:13:51 -0300

    add button in add new comment form

--- frontEnd/src/Components/ui/Comments/NewCommentForm.tsx
+++ frontEnd/src/Components/ui/Comments/NewCommentForm.tsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 
 export default function NewCommentForm() {
   
   const [content, setContent] = useState('');
 
+  const handleComment = ({preventDefault}: FormEvent) => {
+    preventDefault();
+    setContent('');
+  };
+
   return (
     <form>
-      <label>
+      <label htmlFor='content'>
         <input
           value={content}
+          name="content"
           onChange={({target}) => setContent(target.value)}
           type={'text'}
         />
       </label>
+
+      <button onClick={handleComment}>
+        Enviar
+      </button>
     </form>
   );
 }
