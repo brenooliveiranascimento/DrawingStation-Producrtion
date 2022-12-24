5db1c4d09a63e50d0348ad8643f447a675e3eb49
Breno Nascimento <satsukebare23@gmail.com>
2022-12-24 13:07:43 -0300

    add show video in player

--- frontEnd/src/Components/ui/Player/player.tsx
+++ frontEnd/src/Components/ui/Player/player.tsx
@@ -20,7 +20,7 @@ export default function Player() {
   return (
     <section className={styles.player}>
       <iframe width="560" height="315"
-        src={'https://www.youtube.com/embed/'}
+        src={`https://www.youtube.com/embed/${classroom.video}`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
       ></iframe>
