7b902405312d1f21b433bf80badbc51fdd4e7d82
Breno Nascimento <satsukebare23@gmail.com>
2022-12-29 11:33:12 -0300

    add classrooms in homepage

--- frontEnd/src/Components/ui/ModulesScreen/ModulesScreen.tsx
+++ frontEnd/src/Components/ui/ModulesScreen/ModulesScreen.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { ClassroomInterface } from '../../../interfaces/modules/classroomInterface';
 import { globalState } from '../../../interfaces/modules/globalStateInterface';
-import { ModulesInterface } from '../../../interfaces/modules/ModulesInterface';
+import { ModulesInterface, SubModuleInterface } from '../../../interfaces/modules/ModulesInterface';
 import ModuleCard from './ModuleCard';
+import ClassCard from './classCard';
 import styles from './styles.module.scss';
 
 export default function ModulesScreen() {
-  const { modules } = useSelector((state: globalState) => state);
+  const { modules, subModules: { subModules } } = useSelector((state: globalState) => state);
   console.log(modules);
   return (
     <section className={styles.module_container}>
-      <section>
-        { modules.modules.map((currModule: ModulesInterface) => (
-          <ModuleCard moduleCard={currModule} key={currModule.id}/>
-        )) }
+      <section style={{
+        display: 'flex', flexDirection: 'column'
+      }}>
+        <h1>Módulos</h1>
+        <section style={{
+          display: 'flex', flexDirection: 'row'
+        }}>
+          { modules.modules.map((currModule: ModulesInterface) => (
+            <ModuleCard moduleCard={currModule} key={currModule.id}/>
+          )) }
+        </section>
+      </section>
+
+      <section style={{
+        display: 'flex', flexDirection: 'column'
+      }}>
+        <h1>Aulas</h1>
+        <section style={{
+          display: 'flex', flexDirection: 'row'
+        }}>
+          { subModules.map((currSubModule: SubModuleInterface) => {
+            return currSubModule.classrooms.map((currClass: ClassroomInterface) => (
+              <ClassCard key={currClass.id} classInf={currClass} subModule={currSubModule.name} />
+            ));
+          }) }
+        </section>
       </section>
     </section>
   );
---
+++ frontEnd/src/Components/ui/ModulesScreen/classCard.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import React from 'react';
+import { ClassroomInterface } from '../../../interfaces/modules/classroomInterface';
+import styles from './styles.module.scss';
+
+interface IClassCardProps {
+  classInf: ClassroomInterface,
+  subModule: string
+}
+
+export default function ClassCard({ classInf, subModule }: IClassCardProps) {
+  const { image, name, conclude, premium, id } = classInf;
+  return (
+    <section className={styles.module_card_container}>
+      <section className={styles.image_area}>
+        <Image
+          style={{objectFit: 'cover'}}
+          width={350} height={200} src={image} alt={name} />
+      </section>
+      <article>
+        <h2>{name}</h2>
+        <p>{subModule}</p>
+      </article>
+      <section>
+      </section>
+      <button type='button'>
+        <span>
+        Continuar assistindo
+        </span>
+      </button>
+    </section>
+  );
+}
