164d56776d5238a6a58e7ced6d9f1071b79711a4
Breno Nascimento <satsukebare23@gmail.com>
2023-01-04 13:41:48 -0300

    add update notification funcionality

--- backEnd/src/routes/notifications.ts
+++ backEnd/src/routes/notifications.ts
@@ -7,5 +7,6 @@ const notificationController = new NotificationController();
 
 router.get('/get/:id', (req, res) => notificationController.get(req, res));
 router.post('/create', (req, res) => notificationController.create(req, res));
+router.post('/update/:id', (req, res) => notificationController.update(req, res));
 
 export default router;
