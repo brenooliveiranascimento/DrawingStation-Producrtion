4cbcab8ebdeb5eb6186a2edb4386bbda73b23dfc
Breno Nascimento <satsukebare23@gmail.com>
2023-01-04 15:03:57 -0300

    add route

--- backEnd/src/routes/notifications.ts
+++ backEnd/src/routes/notifications.ts
@@ -8,5 +8,7 @@ const notificationController = new NotificationController();
 router.get('/get/:id', (req, res) => notificationController.get(req, res));
 router.post('/create', (req, res) => notificationController.create(req, res));
 router.post('/update/:id', (req, res) => notificationController.update(req, res));
+router.delete('/delete/one', (req, res) => notificationController.deleteOne(req, res));
+router.delete('/delete/all', (req, res) => notificationController.deleteAll(req, res));
 
 export default router;
