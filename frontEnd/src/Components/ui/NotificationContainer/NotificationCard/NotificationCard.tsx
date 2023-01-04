fce1d7e1430d6fc206095d49b090e365afe39c13
Breno Nascimento <satsukebare23@gmail.com>
2023-01-04 18:52:29 -0300

    add redirect user to class with comment response

--- frontEnd/src/Components/ui/NotificationContainer/NotificationCard/NotificationCard.tsx
+++ frontEnd/src/Components/ui/NotificationContainer/NotificationCard/NotificationCard.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { globalState } from '../../../../interfaces/modules/globalStateInterface';
 import { INotification } from '../../../../interfaces/modules/notificationInterfaces';
 import styles from './styles.module.scss';
-import {SubModuleInterface} from '../../../../interfaces/modules/ModulesInterface';
+import {ModulesInterface, SubModuleInterface} from '../../../../interfaces/modules/ModulesInterface';
 import { ClassroomInterface } from '../../../../interfaces/modules/classroomInterface';
+import { selectClassroomAction, selectSubModuleAction } from '../../../../redux/actions/classroomControllerActions/ClassroomControllerAciton';
+import { handleScreen } from '../../../../redux/actions/genericActions';
+import { Router, useRouter } from 'next/router';
+import { selectCurrSubModule } from '../../../../redux/actions/classroomControllerActions/genericActions';
 
 interface INotificationCardProps {
   notification: INotification
@@ -14,7 +18,9 @@ interface INotificationCardProps {
 export default function NotificationCard({ notification }: INotificationCardProps) {
   const { active, classData, content, senderData, type } = notification;
 
-  const { subModules: {subModules} } = useSelector((state:globalState) => state);
+  const { subModules: {subModules}, modules: { modules } } = useSelector((state:globalState) => state);
+  const dispatch = useDispatch();
+  const router = useRouter();
 
   const findSubModule = () => {
     return subModules.find((currSubmodule: SubModuleInterface) =>
@@ -22,14 +28,36 @@ export default function NotificationCard({ notification }: INotificationCardProp
   };
 
   const findClass = () => {
-    return subModules.find((currSubmodule: SubModuleInterface) =>
-      currSubmodule.classrooms.find((currClass: ClassroomInterface) => {
-        return currClass.id === classData.id;
-      }) );
+    const currSub = subModules.find((currSubmodule: SubModuleInterface) =>
+      currSubmodule.id === classData.subModuleId );
+    const currClassData = currSub.classrooms.find((currClass: ClassroomInterface) => {
+      return currClass.id === classData.id;
+    });
+    console.log(currClassData);
+    return currClassData;
   };
 
+  const selectModule = (moduleInfo: ModulesInterface) => {
+    dispatch(selectSubModuleAction(moduleInfo));
+    dispatch(handleScreen('Classroom'));
+    router.push('/Classroom');
+  };
+
+
+  const selectClass = (classInfos: ClassroomInterface) => {
+    dispatch(selectClassroomAction(classInfos));
+  };
+
+  const selectSubModule = () => {
+    const findCurrModule = modules.find((currModule: ModulesInterface) => currModule.id === findSubModule().moduleId);
+    selectModule(findCurrModule);
+    dispatch(selectCurrSubModule({ name: findSubModule().name, id: findSubModule().id }));
+    selectClass(findClass());
+  };
+
+
   return (
-    <article onClick={() => alert('fdnoi')} className={styles.notification_card_container}>
+    <article onClick={selectSubModule} className={styles.notification_card_container}>
       <aside className={styles.user_side}>
         <section>
           <Image
