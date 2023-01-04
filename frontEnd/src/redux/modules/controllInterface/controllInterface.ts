514e0ac8fd6ae191946387c8edc54ab6d864d26f
Breno Nascimento <satsukebare23@gmail.com>
2023-01-04 19:02:47 -0300

    create controller inteface

---
+++ frontEnd/src/redux/modules/controllInterface/controllInterface.ts
@@ -0,0 +1,14 @@
+export const controllInterfaceInitialValue = {
+  toComments: false,
+};
+
+export const controllInterfaceAction = {
+  type: '',
+  payload: false
+};
+
+export default function controllInterface(
+  state = controllInterfaceInitialValue, action = controllInterfaceAction
+) {
+
+}
