fd1310587c7dae464022d8dbf3f01f9bc15fbed1
Breno Nascimento <satsukebare23@gmail.com>
2023-01-04 13:39:38 -0300

    add update notification service

---
+++ backEnd/src/services/notification/Update.ts
@@ -0,0 +1,13 @@
+import NotificationsModel from "../../database/models/NotificationModel";
+import CustomError from "../../utils/StatusError";
+
+export default class Update {
+  async execute(id: number) {
+    try {
+      await NotificationsModel
+      .update({ active: false }, { where: { userId: id } })
+    } catch(e: any) {
+      throw new CustomError(e.message, 500);
+    }
+  }
+}
