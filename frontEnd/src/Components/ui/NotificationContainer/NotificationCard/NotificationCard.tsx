5693a60d922116585c85c50e344cdf3e40615904
Breno Nascimento <satsukebare23@gmail.com>
2023-01-04 17:45:47 -0300

    add show new notifications

--- frontEnd/src/Components/ui/NotificationContainer/NotificationCard/NotificationCard.tsx
+++ frontEnd/src/Components/ui/NotificationContainer/NotificationCard/NotificationCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { INotification } from '../../../../interfaces/modules/notificationInterfaces';
+import styles from './styles.module.scss';
 
 interface INotificationCardProps {
   notification: INotification
@@ -7,9 +8,11 @@ interface INotificationCardProps {
 
 export default function NotificationCard({ notification }: INotificationCardProps) {
   const { active, classData, content, senderData, type } = notification;
+
   return (
-    <article>
-      {senderData.name}
+    <article className={styles.notification_card_container}>
+      <h1>{senderData.name}</h1>
+      <span>{active && 'novo!!'}</span>
     </article>
   );
 }
--- frontEnd/src/Components/ui/NotificationContainer/NotificationContainer.tsx
+++ frontEnd/src/Components/ui/NotificationContainer/NotificationContainer.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { globalState } from '../../../interfaces/modules/globalStateInterface';
 import { INotification } from '../../../interfaces/modules/notificationInterfaces';
+import { updateNotification } from '../../../redux/actions/notificationActions/updateNotificationAction';
 import NotificationCard from './NotificationCard/NotificationCard';
 import styles from './styles.module.scss';
 
@@ -16,12 +17,22 @@ export default function NotificationContainer({active}: INotificationProps) {
     notificationsModule: { data, error, errorMessage }
   } = useSelector((state: globalState) => state);
 
+  const dispatch = useDispatch();
+
+  const [newData, setNewData] = useState<any>([]);
+
+  useEffect(() => {
+    if(!active) return;
+    setNewData(data);
+    dispatch(updateNotification());
+  }, [active]);
+
   return (
     <section
       style={{ height: active ? '600px' : '0px' }}
       className={styles.notification_container} >
       {
-        data.map((currNotification: INotification) =>
+        newData.reverse().map((currNotification: INotification) =>
           <NotificationCard notification={currNotification} key={currNotification.id}/>)
       }
     </section>
