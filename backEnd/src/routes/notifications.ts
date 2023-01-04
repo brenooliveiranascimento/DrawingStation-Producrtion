237d1935a6f8c4b95ccdb2500026d8c747384c1a
Breno Nascimento <satsukebare23@gmail.com>
2023-01-04 15:24:09 -0300

    add token validation in notification route

--- backEnd/src/routes/notifications.ts
+++ backEnd/src/routes/notifications.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import validateToken from '../middlewares/tokenVerification';
 import NotificationController from '../services/notification/NotificationController';
 
 const router = Router()
 
 const notificationController = new NotificationController();
+router.use(validateToken);
 
 router.get('/get/:id', (req, res) => notificationController.get(req, res));
 router.post('/create', (req, res) => notificationController.create(req, res));
