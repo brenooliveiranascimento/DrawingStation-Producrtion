3497c1bcaa3a427ea6c6c3eb439c97c0d2f975c3
Breno Nascimento <satsukebare23@gmail.com>
2023-01-04 12:02:17 -0300

    create mount notifications data with sender data and class origin data

--- backEnd/src/services/notification/Get.ts
+++ backEnd/src/services/notification/Get.ts
@@ -1,12 +1,31 @@
 import NotificationsModel from "../../database/models/NotificationModel";
 import statusCodes from "../../statusCode";
 import CustomError from "../../utils/StatusError";
+import ClassroomModel from "../../database/models/ClassroomModel";
+import UserModel from "../../database/models/UserModel";
 
 export default class Get {
+
+  async mountNotifications(notifications: NotificationsModel[]) {
+    const data = Promise.all(notifications
+      .map(async (currNotificaiton: NotificationsModel) => {
+        const senderData = await UserModel
+          .findOne({ where: { id: currNotificaiton.senderId }})
+        const classOrigin = await ClassroomModel.findOne({
+          where: { id: currNotificaiton.classroomId }
+        });
+        return { ...currNotificaiton, senderData, classOrigin }
+      }))
+  }
+
   async execute(id: number) {
     try {
-      const notifications = await NotificationsModel
+
+    const notifications = await NotificationsModel
       .findAll({ where: { userId: id } })
+
+    const notificationsData = this.mountNotifications(notifications);
+
     return notifications;
     } catch (e: any) {
       throw new CustomError(e.message, 500);
