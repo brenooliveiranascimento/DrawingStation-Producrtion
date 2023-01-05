83a9534db12e85fed312999837597ef3eb3815ad
Breno Nascimento <satsukebare23@gmail.com>
2023-01-05 15:24:10 -0300

    clean seeders

--- backEnd/src/database/seeders/01-users.js
+++ backEnd/src/database/seeders/01-users.js
@@ -3,36 +3,36 @@ module.exports = {
     await queryInterface.bulkInsert(
       'users',
       [
-        {
-          name: 'Breno Nascimento',
-          email: 'breno564nascimwnto@gmail.com',
-          password: 'djwi8u290ue9i8wj893qwjr093qjr',
-          active: true,
-          premium: true,
-          loginType: 'google',
-          birthday: '2002-02-23',
-          phoneNumber: 'xx-xxxxx-xx'
-        },
-        {
-          name: 'UserTeste',
-          email: 'Teste@gmail.com',
-          password: 'jawpjdiwa0idopawjcdfwaf',
-          active: true,
-          loginType: 'credential',
-          premium: true,
-          birthday: '2002-02-23',
-          phoneNumber: '8492384932409234'
-        },
-        {
-          name: 'Teste2@gmail.com',
-          email: 'teste2@gmail.com',
-          password: 'djwi8u290ue9i8wj893qwjr093qjr',
-          active: true,
-          loginType: 'credential',
-          premium: true,
-          birthday: '2002-02-23',
-          phoneNumber: '77988439270'
-        },
+        // {
+        //   name: 'Breno Nascimento',
+        //   email: 'breno564nascimwnto@gmail.com',
+        //   password: 'djwi8u290ue9i8wj893qwjr093qjr',
+        //   active: true,
+        //   premium: true,
+        //   loginType: 'google',
+        //   birthday: '2002-02-23',
+        //   phoneNumber: 'xx-xxxxx-xx'
+        // },
+        // {
+        //   name: 'UserTeste',
+        //   email: 'Teste@gmail.com',
+        //   password: 'jawpjdiwa0idopawjcdfwaf',
+        //   active: true,
+        //   loginType: 'credential',
+        //   premium: true,
+        //   birthday: '2002-02-23',
+        //   phoneNumber: '8492384932409234'
+        // },
+        // {
+        //   name: 'Teste2@gmail.com',
+        //   email: 'teste2@gmail.com',
+        //   password: 'djwi8u290ue9i8wj893qwjr093qjr',
+        //   active: true,
+        //   loginType: 'credential',
+        //   premium: true,
+        //   birthday: '2002-02-23',
+        //   phoneNumber: '77988439270'
+        // },
       ],
       {},
     );
--- backEnd/src/database/seeders/03-sub-modules.js
+++ backEnd/src/database/seeders/03-sub-modules.js
@@ -3,27 +3,27 @@ module.exports = {
     await queryInterface.bulkInsert(
       'sub_modules',
       [
-        {
-          name: 'Fundamentos',
-          description: 'Hora de por a mão na massa!',
-          image: 'https://instagram.fgnm2-1.fna.fbcdn.net/v/t51.2885-15/273804194_477832993917681_7237425992266053249_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=instagram.fgnm2-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=eg_Ps-A_DdIAX8NUHvQ&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc3MjE0NzMzNDg4MjY4MTgyOQ%3D%3D.2-ccb7-5&oh=00_AfCxQPRRNZZpM05FCYcFAq4FPdtH6JIKh8XzXZsjdOhTQQ&oe=637674AC&_nc_sid=30a2ef',
-          premium: false,
-          module_id:1
-        },
-        {
-          name: 'Rosto',
-          description: 'Fundamentos do esbolo',
-          premium: true,
-          image: 'https://instagram.fgnm2-1.fna.fbcdn.net/v/t51.2885-15/273804194_477832993917681_7237425992266053249_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=instagram.fgnm2-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=eg_Ps-A_DdIAX8NUHvQ&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc3MjE0NzMzNDg4MjY4MTgyOQ%3D%3D.2-ccb7-5&oh=00_AfCxQPRRNZZpM05FCYcFAq4FPdtH6JIKh8XzXZsjdOhTQQ&oe=637674AC&_nc_sid=30a2ef',
-          module_id:1,
-        },
-        {
-          name: 'Metais',
-          description: 'Detalhes e recomendação de materiais',
-          image: 'https://instagram.fgnm2-1.fna.fbcdn.net/v/t51.2885-15/273804194_477832993917681_7237425992266053249_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=instagram.fgnm2-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=eg_Ps-A_DdIAX8NUHvQ&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc3MjE0NzMzNDg4MjY4MTgyOQ%3D%3D.2-ccb7-5&oh=00_AfCxQPRRNZZpM05FCYcFAq4FPdtH6JIKh8XzXZsjdOhTQQ&oe=637674AC&_nc_sid=30a2ef',
-          premium: true,
-          module_id:1,
-        },
+        // {
+        //   name: 'Fundamentos',
+        //   description: 'Hora de por a mão na massa!',
+        //   image: 'https://instagram.fgnm2-1.fna.fbcdn.net/v/t51.2885-15/273804194_477832993917681_7237425992266053249_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=instagram.fgnm2-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=eg_Ps-A_DdIAX8NUHvQ&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc3MjE0NzMzNDg4MjY4MTgyOQ%3D%3D.2-ccb7-5&oh=00_AfCxQPRRNZZpM05FCYcFAq4FPdtH6JIKh8XzXZsjdOhTQQ&oe=637674AC&_nc_sid=30a2ef',
+        //   premium: false,
+        //   module_id:1
+        // },
+        // {
+        //   name: 'Rosto',
+        //   description: 'Fundamentos do esbolo',
+        //   premium: true,
+        //   image: 'https://instagram.fgnm2-1.fna.fbcdn.net/v/t51.2885-15/273804194_477832993917681_7237425992266053249_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=instagram.fgnm2-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=eg_Ps-A_DdIAX8NUHvQ&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc3MjE0NzMzNDg4MjY4MTgyOQ%3D%3D.2-ccb7-5&oh=00_AfCxQPRRNZZpM05FCYcFAq4FPdtH6JIKh8XzXZsjdOhTQQ&oe=637674AC&_nc_sid=30a2ef',
+        //   module_id:1,
+        // },
+        // {
+        //   name: 'Metais',
+        //   description: 'Detalhes e recomendação de materiais',
+        //   image: 'https://instagram.fgnm2-1.fna.fbcdn.net/v/t51.2885-15/273804194_477832993917681_7237425992266053249_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=instagram.fgnm2-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=eg_Ps-A_DdIAX8NUHvQ&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc3MjE0NzMzNDg4MjY4MTgyOQ%3D%3D.2-ccb7-5&oh=00_AfCxQPRRNZZpM05FCYcFAq4FPdtH6JIKh8XzXZsjdOhTQQ&oe=637674AC&_nc_sid=30a2ef',
+        //   premium: true,
+        //   module_id:1,
+        // },
       ],
       {},
     );
