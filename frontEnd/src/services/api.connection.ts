4cbe232f31fb94e1058ccdf98eeddbc8861c543a
Breno Nascimento <satsukebare23@gmail.com>
2023-01-05 16:20:08 -0300

    change endpoint

--- frontEnd/src/services/api.connection.ts
+++ frontEnd/src/services/api.connection.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const apiConnection = axios.create({
-  baseURL: 'backend-production-5574.up.railway.app',
+  baseURL: 'https://drawingstation-backend-production-5574.up.railway.app/',
   // baseURL: 'http://localhost:3001',
 });
 
 export const serverSideConnection = axios.create({
-  baseURL: 'backend-production-5574.up.railway.app',
+  baseURL: 'https://drawingstation-backend-production-5574.up.railway.app/',
   // baseURL: 'http://localhost:3001',
 });
