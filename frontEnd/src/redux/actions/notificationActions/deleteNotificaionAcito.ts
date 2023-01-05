fc88f64d6ea44cf6634abac52b5580b4379d9ad2
Breno Nascimento <satsukebare23@gmail.com>
2023-01-04 21:23:54 -0300

    create delete all notification in backend action

---
+++ frontEnd/src/redux/actions/notificationActions/deleteNotificaionAcito.ts
@@ -0,0 +1,31 @@
+import { parseCookies } from 'nookies';
+import { Dispatch } from 'react';
+import { toast } from 'react-toastify';
+import { globalState } from '../../../interfaces/modules/globalStateInterface';
+import { apiConnection } from '../../../services/api.connection';
+import { NotificationTypes } from '../../Types/AuthTypes';
+import { genericNotificationAction } from './genericNotificaitonActions';
+
+export const deleteAllNotification = (): any => {
+  return async (dispatch: Dispatch<any>, state: () => globalState) => {
+    const { userData } = state().user;
+    if(!userData.name.length) return;
+    const cookies = parseCookies();
+    const token = cookies['DRAWING_USER_DATA'];
+    try {
+      await apiConnection.get('/notification/delete/all', 
+        {
+          userId: userData.id
+        },
+        {
+          headers: { 'Authorization': token }
+        });
+      dispatch(genericNotificationAction(
+        NotificationTypes.DELETE_NOTIFICATIONS,
+        null));
+    } catch(e: any) {
+      console.log(e);
+      toast.error(e.message);
+    }
+  };
+};
