2fbf0a2644dbfa5ef76c3c8e5704580ff8a75b07
Breno Nascimento <satsukebare23@gmail.com>
2023-01-05 10:39:27 -0300

    add block in request comments

--- frontEnd/src/redux/actions/commentsActions/commentsActions.ts
+++ frontEnd/src/redux/actions/commentsActions/commentsActions.ts
@@ -2,13 +2,17 @@ import { parseCookies } from 'nookies';
 import { Dispatch } from 'react';
 import { toast } from 'react-toastify';
 import { globalState } from '../../../interfaces/modules/globalStateInterface';
+import { SubModuleInterface } from '../../../interfaces/modules/ModulesInterface';
 import { apiConnection } from '../../../services/api.connection';
 import { requestComments } from './genericAtions';
 
 export const requestSubCommentsAction = (): any => {
   return async (dispatch: Dispatch<any>, state: () => globalState) => {
-    const { classroomController: { classroom } } = state();
-    // if(commentsModule.comments?.length) return;
+    const { classroomController: { classroom, subModules, currSubModule } } = state();
+    
+    if(!subModules.find((subM:SubModuleInterface) =>
+      subM.id === currSubModule.id)?.classrooms.length) return;
+    
     const cookies = parseCookies();
     const token = cookies['DRAWING_USER_DATA'];
     try {
