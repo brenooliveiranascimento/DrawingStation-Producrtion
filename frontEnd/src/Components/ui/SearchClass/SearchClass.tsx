cbcde2f5569dd425e23192a5ea74812ef3afad97
Breno Nascimento <satsukebare23@gmail.com>
2023-01-06 20:31:10 -0300

    add state and ghet store in search calssroom component

--- frontEnd/src/Components/ui/SearchClass/SearchClass.tsx
+++ frontEnd/src/Components/ui/SearchClass/SearchClass.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import { ClassroomInterface } from '../../../interfaces/modules/classroomInterface';
+import { globalState } from '../../../interfaces/modules/globalStateInterface';
 
 export default function SearchClass() {
+
+  const {
+    classroomController: { classroom, subModules },
+  } = useSelector((state: globalState) => state);
+
+  const [classrooms, setClassrooms] = useState<ClassroomInterface[]>([]);
+
+  const searchClassrooms = () => {
+  };
+
   return (
     <div
       style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}
