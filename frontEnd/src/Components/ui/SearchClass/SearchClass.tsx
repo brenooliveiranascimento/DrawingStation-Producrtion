02399fdc20f1e6f7c1c238ca0dde51ba388740a8
Breno Nascimento <satsukebare23@gmail.com>
2023-01-06 20:35:30 -0300

    add get classrroms funcionality in search component

--- frontEnd/src/Components/ui/SearchClass/SearchClass.tsx
+++ frontEnd/src/Components/ui/SearchClass/SearchClass.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { ClassroomInterface } from '../../../interfaces/modules/classroomInterface';
 import { globalState } from '../../../interfaces/modules/globalStateInterface';
+import { SubModuleInterface } from '../../../interfaces/modules/ModulesInterface';
 
 export default function SearchClass() {
 
@@ -11,15 +12,29 @@ export default function SearchClass() {
   } = useSelector((state: globalState) => state);
 
   const [classrooms, setClassrooms] = useState<ClassroomInterface[]>([]);
+  const [className, setClassName] = useState('');
 
   const searchClassrooms = () => {
+    const getClass = subModules.reduce((
+      acc: ClassroomInterface[], currValue: SubModuleInterface) => {
+      return [...acc, currValue.classrooms];
+    }, []).filter((currClassroom: ClassroomInterface) => currClassroom.name.includes(className));
+    setClassrooms(getClass);
   };
+
+  useEffect(() => {
+    searchClassrooms();
+  }, [className]);
 
   return (
     <div
       style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}
     >
-      <input placeholder='Procurar Aula'/>
+      <input
+        value={className}
+        onChange={({target}) => setClassName(target.value)}
+        placeholder='Procurar Aula'
+      />
       <FaSearch color='#blue'/>
     </div>
   );
