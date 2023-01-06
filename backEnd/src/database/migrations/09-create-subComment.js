eb9f1ff97f2f8187493835b3c6c721a9af76124f
Breno Nascimento <satsukebare23@gmail.com>
2023-01-06 17:26:38 -0300

    add max height in notification card

--- backEnd/src/database/migrations/09-create-subComment.js
+++ backEnd/src/database/migrations/09-create-subComment.js
@@ -9,7 +9,7 @@ module.exports = {
       },
       content: {
         allowNull: false,
-        type: Sequelize.STRING(500),
+        type: Sequelize.STRING(1000),
       },
       active: {
         allowNull: false,
